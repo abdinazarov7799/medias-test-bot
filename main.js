3d69a3c7c2dc7397038449136b27b220a6b3c970
abdinazarov7799 <parolxato@gmail.com>
2024-01-25 12:07:53 +0500

    .

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api');
+const {isEqual} = require("lodash");
+const { Sequelize } = require('sequelize');
 
 const token = '6556043828:AAGTY4f7KnLaYnq7SSIDHaCGIqwlMW7EVcg';
-
+const webAppUrl = 'https://mediasolutions.uz/';
 const bot = new TelegramBot(token ,{ polling: true });
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
+    const text = msg.text;
 
-    bot.sendMessage(chatId,'aa');
+    if (isEqual(text,'/start')){
+        await bot.sendMessage(chatId,'Xush kelibsiz',{
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Ilovani ochish', web_app: {url: webAppUrl}}]
+                ]
+            }
+        });
+    }
 })
